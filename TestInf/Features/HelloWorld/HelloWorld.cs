18cd86049fc3305234fe2dfdf518c3be1393bc08
Xiangwei Tang <tangxiangwei@gmail.com>
2019-06-10 21:51:56 +0800

    Construct the entrance.

---
+++ TestInf/Features/HelloWorld/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestInf.Features
+{
+    class HelloWorld : TestInf
+    {
+        public override void Run()
+        {
+            
+        }
+    }
+}
--- TestInf/Program.cs
+++ TestInf/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TestInf
 {
@@ -11,13 +12,32 @@ namespace TestInf
         static Features.DummyConfig MainCfg = new Features.DummyConfig();
         static void Main(string[] args)
         {
-            PrimaryLoad(args);
+            LoadAndRun(args);
         }
 
-        static void PrimaryLoad(string[] args)
+        static void LoadAndRun(string[] args)
         {
             var arg = new TestInfArgs(args);
             MainCfg.Load(arg);
-        }        
+            foreach(string taskName in MainCfg.DecomposeTasks())
+            {
+                TestInfArgs newArg = MainCfg.CreateNewArg(taskName);
+                var task = GetTask(taskName);
+                task.LoadAndRun(newArg);
+            }
+        }
+
+        static Features.TestInf GetTask(string taskName)
+        {
+            switch (taskName.ToLower())
+            {
+                case "na":
+                    return new Features.NA();
+                case "helloworld":
+                    return new Features.HelloWorld();
+                default:
+                    throw new TestInfEx($"Invalid task name {taskName}.");
+            }
+        }
     }
 }
