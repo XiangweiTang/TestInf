98f71c43792d66b8237d8bdfa7ac6eee58f18013
Xiangwei Tang <v-xianta@microsoft.com>
2019-06-10 03:20:18 -0700

    Update. Reset arg structure.

--- TestInf/Common/TestInfArgs.cs
+++ TestInf/Common/TestInfArgs.cs
@@ -10,7 +10,7 @@ namespace TestInf
     {
         public TaskMode Mode { get; private set; } = TaskMode.NA;
         public string ConfigPath { get; private set; } = "";
-        public Dictionary<string, string> ArgDict { get; private set; } = new Dictionary<string, string>();
+        public Dictionary<string, string[]> ArgDict { get; private set; } = new Dictionary<string, string[]>();
         public TestInfArgs() { }
         public TestInfArgs(string[] args)
         {
@@ -26,18 +26,18 @@ namespace TestInf
                 case "magictest":
                     Mode |= TaskMode.Test;
                     Mode |= TaskMode.Argument;
-                    SetArgDict(args.Skip(1));
+                    SetArgDict(args.Skip(1).ToArray());
                     break;
                 case "magicschedule":
                     Mode |= TaskMode.Config;
                     Mode |= TaskMode.Argument;
                     Sanity.Requires(args.Length >= 2, "Schedule mode requires at least two args.");
                     ConfigPath = args[1];
-                    SetArgDict(args.Skip(2));
+                    SetArgDict(args.Skip(2).ToArray());
                     break;
                 case "magicarg":
                     Mode |= TaskMode.Argument;
-                    SetArgDict(args.Skip(1));
+                    SetArgDict(args.Skip(1).ToArray());
                     break;
                 default:
                     Mode |= TaskMode.Config;
@@ -46,35 +46,30 @@ namespace TestInf
             }
         }
 
-        private void SetArgDict(IEnumerable<string> args)
+        private void SetArgDict(string[] args)
         {
-            // -OPT0 ARG00 ARG01 ... -OPT1 ARG10 ARG11 ...
-            string key = "";
-            List<string> values = new List<string>();
+            // FreeArg0 FreeArg1 ... -OPT0 Arg00 Arg01 ... -OPT1 Arg10 Arg11 ...
+            /*
+             * Key: ""  Value: {FreeArg0, FreeArg1, ...}
+             * Key: "opt0"  Value: {Arg00, Arg01, ...}
+             * Key: "opt1"  Value: {Arg10, Arg11, ...}
+             */
+
+            string key = "";            
+            List<string> currentArgList = new List<string>();
             foreach(string arg in args)
             {
                 if (arg[0] == '-')
                 {
-                    if (!string.IsNullOrEmpty(key))
-                    {
-                        Sanity.Requires(!ArgDict.ContainsKey(key), $"Duplicate in options: -{key}.");
-                        string value = string.Join(" ", values);                        
-                        ArgDict.Add(key, value);
-                        values.Clear();
-                    }
-                    key = arg.Substring(1);
+                    ArgDict.Add(key, currentArgList.ToArray());
+                    currentArgList.Clear();
+                    key = arg.Substring(1).ToLower();
+                    Sanity.Requires(!ArgDict.ContainsKey(key), $"Duplication option {arg[0]}.");
+                    continue;
                 }
-                else
-                {
-                    values.Add(arg);
-                }
+                currentArgList.Add(arg);
             }
-            if (!string.IsNullOrEmpty(key))
-            {
-                Sanity.Requires(!ArgDict.ContainsKey(key), $"Duplicate in options: -{key}.");
-                string value = string.Join(" ", values);
-                ArgDict.Add(key, value);
-            }
+            ArgDict.Add(key, currentArgList.ToArray());
         }
     }
 }
